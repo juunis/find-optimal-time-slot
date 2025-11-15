4ed9839398a8a78666281d813a0a62e69660e530
juunis <juunis@hotmail.co.uk>
2025-11-15 20:12:28 +0200

    Modified build.sh script

--- optimal_time_slot_lambda/build.sh
+++ optimal_time_slot_lambda/build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # Exit if any of the following commands exit with non-0, and echo our commands back
-set -ex
-
-# Paths
-LAMBDA_SRC_DIR="optimal-time-slot/src"
-DIST_DIR="optimal-time-slot/dist"
-ZIP_FILE="$DIST_DIR/lambda.zip"
+set -ex"
 
 # Zip all files from src into dist/lambda.zip
 # Exclude __pycache__ folders and .pyc files
-(cd "$LAMBDA_SRC_DIR" && zip -r "$ZIP_FILE" . -x "*.pyc" -x "*/__pycache__/*")
+(cd src && zip -r dist/lambda.zip . -x "*.pyc" -x "*/__pycache__/*")
