4fadc11869b98a11adabf821f08dbec83016f8db
juunis <juunis@hotmail.co.uk>
2025-11-15 20:15:45 +0200

    Removed accidental quotation marks from script

--- optimal_time_slot_lambda/build.sh
+++ optimal_time_slot_lambda/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit if any of the following commands exit with non-0, and echo our commands back
-set -ex"
+set -ex
 
 # Zip all files from src into dist/lambda.zip
 # Exclude __pycache__ folders and .pyc files
